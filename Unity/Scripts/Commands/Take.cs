3928d6f44a6a8168c7ce07197fa026004213a7cd
Killer-Kat <goldkat99@gmail.com>
2023-07-01 17:48:01 -0400

    Refactor take command
    decided to just extend take command functionality instead of insisting on explicit take from container command.

--- Unity/Scripts/Commands/Take.cs
+++ Unity/Scripts/Commands/Take.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,37 @@ public class Take : InputAction
     {
         combinedInputWords = "";
         Debug.Log(separatedInputWords.Length);
+
+        if(Array.Find(separatedInputWords, element => element == "from") == "from")
+        {
+            string itemToTake = "";
+            string containerToLoot = "";
+            int x = Array.IndexOf(separatedInputWords, "from");
+            Debug.Log("from found at index " + x);
+            if(x > 2)
+            {
+                for (int i = 1; i < x; i++)
+                {
+                    itemToTake = itemToTake + " " + separatedInputWords[i];
+                }
+                Debug.Log(itemToTake);
+
+            }
+            else
+            {
+                itemToTake = separatedInputWords[1];
+            }
+            for (int i = x + 1; i < separatedInputWords.Length; i++)
+            {
+                containerToLoot = containerToLoot + " " + separatedInputWords[i];
+            }
+            Debug.Log(containerToLoot);
+            for (int i = 0; i < controller.roomNavigation.currentRoom.InteractableObjectsInRoom.Count; i++)
+            {
+
+            }
+        }
+
         if(separatedInputWords.Length > 1)
         {
             for (int i = 1; i < separatedInputWords.Length; i++)
@@ -30,7 +62,7 @@ public class Take : InputAction
                 controller.roomNavigation.currentRoom.InteractableObjectsInRoom.RemoveAt(i);
                 controller.LogStringWithReturn("You take the " + combinedInputWords);
                 return;
-            }
+            } 
         }
         controller.LogStringWithReturn(combinedInputWords + " not found.");
     }
--- Unity/Scripts/Container.cs
+++ Unity/Scripts/Container.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(menuName = "TextAdventure/Container")]
 public class Container : InteractableObject
 {
+    [SerializeField]
     List<InteractableObject> contents;
 }
