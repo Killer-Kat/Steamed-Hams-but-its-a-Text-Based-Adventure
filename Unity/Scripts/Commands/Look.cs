10f5caca88646d9d0c8f2dcb0075bef7a9ca1ac2
Killer-Kat <goldkat99@gmail.com>
2023-07-31 17:35:33 -0400

    Finished Look

--- Unity/Scripts/Commands/Look.cs
+++ Unity/Scripts/Commands/Look.cs
@@ -9,6 +9,34 @@ public class Look : InputAction
         if (separatedInputWords.Length <= 1)
         {
             controller.DisplayRoomText();
+        } else if (separatedInputWords.Length == 2)
+        {
+            //Debug.Log("Looking for: " + separatedInputWords[1]);
+            for (int i = 0; i < controller.playerInventory.Count; i++)
+            {
+                //Debug.Log("Look command finds: " + controller.playerInventory[i].noun); 
+                if(separatedInputWords[1].ToLower() == controller.playerInventory[i].noun.ToLower())//noun is a lowercase name used via the parser. .name will give its unity engine name which we do not want
+                {
+                    controller.LogStringWithReturn(controller.playerInventory[i].examineDescription);
+                }
+            } 
+            
+        }else
+        {
+            string itemToFind = "";
+            for (int i = 1; i < separatedInputWords.Length; i++)
+            {
+                itemToFind = itemToFind + " " + separatedInputWords[i];
+            }
+            itemToFind = itemToFind.Substring(1);
+            //Debug.Log("Look searching for: " + itemToFind);
+            for (int i = 0; i < controller.playerInventory.Count; i++)
+            {
+                //Debug.Log("Look command finds: " + controller.playerInventory[i].noun); 
+                if (itemToFind.ToLower() == controller.playerInventory[i].noun.ToLower())//remember to keep everything lowercase!
+                    controller.LogStringWithReturn(controller.playerInventory[i].examineDescription);
+                }
+            }
         }
     }
 }
